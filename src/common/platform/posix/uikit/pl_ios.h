2bac5aa62c3ccd6dd787a40d0782dc8fe00a36bf
baumhoto <baumhoto@gmail.com>
2021-07-29 16:02:04 +0200

    first implementation

---
+++ src/common/platform/posix/uikit/pl_ios.h
@@ -0,0 +1,12 @@
+//
+//  pl_ios.h
+//  pl_ios
+//
+//  Created by Tobias Baumhöver on 29.07.21.
+//
+
+#ifndef pl_ios_h
+#define pl_ios_h
+
+char* GetUserCommandLineFromSettings();
+#endif /* pl_ios_h */
--- src/common/platform/posix/uikit/pl_ios.mm
+++ src/common/platform/posix/uikit/pl_ios.mm
@@ -27,5 +27,18 @@ long GetMaximumFps() {
 }
 
 void openUrl() {
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/baumhoto/gzdoom/blob/master/README.md"]];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/baumhoto/gzdoom/blob/master/INSTALL.md"]];
+}
+
+char* GetUserCommandLineFromSettings() {
+    long cmdIndex = [[NSUserDefaults standardUserDefaults] integerForKey:@"sys_commandlineindex"];
+    if(cmdIndex > 0)
+    {
+        NSString *key = [NSString stringWithFormat:@"sys_commandline%ld", cmdIndex];
+        NSString *commandLine = [[NSUserDefaults standardUserDefaults] stringForKey:key];
+        const char* result = [commandLine UTF8String];
+        return (char*)result;
+    }
+    
+    return NULL;
 }
